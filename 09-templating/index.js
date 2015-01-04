aa40b90b46d381fafb5b65c726f824593ba36153
Rendy Pranata <rpranata@moneytree.jp>
2015-01-04 18:15:47 +0900

    Answer 9

--- 09-templating/index.js
+++ 09-templating/index.js
@@ -6,6 +6,14 @@ var path = require('path');
 var app = module.exports = koa();
 
 app.use(function* () {
+
   var filename = path.join(__dirname, 'homepage.jade');
-  // this.response.body=
+  var htmlBody = yield function(callback){
+    console.log('Thunk Start');
+    jade.renderFile(filename, callback, null);
+    console.log('Thunk End');
+  }
+
+  console.log(htmlBody);
+  this.response.body = htmlBody;
 });
