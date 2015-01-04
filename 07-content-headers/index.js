75ff9de21521acd63067b89c2c39bb023df46df7
Rendy Pranata <rpranata@moneytree.jp>
2015-01-04 17:27:57 +0900

    Answer 7

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -3,6 +3,14 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-  
+app.use(function* (next) {
+    if (!this.request.is('json')) return yield next;
+
+    this.response.type = 'application/json';
+    this.response.body = { message : 'hi!' };
+})
+
+app.use(function* (next) {
+    if (this.request.is('json')) return yield next;
+    this.response.body = 'ok';
 })
